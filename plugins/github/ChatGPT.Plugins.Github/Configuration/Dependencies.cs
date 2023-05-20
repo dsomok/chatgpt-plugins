5cf46c442eaa6dab69a20946914713fab4ab1fa3
Denis Somok <denis.somok@gmail.com>
2023-05-20 22:15:43 +0100

    Fixed missing dependency registration

--- plugins/github/ChatGPT.Plugins.Github/Configuration/Dependencies.cs
+++ plugins/github/ChatGPT.Plugins.Github/Configuration/Dependencies.cs
@@ -1,5 +1,6 @@
 ﻿using ChatGPT.Plugins.Github.Configuration.Models;
 using ChatGPT.Plugins.Github.Handlers;
+using ChatGPT.Plugins.Github.Services.Github;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Octokit;
@@ -15,7 +16,6 @@ public static class Dependencies
     )
     {
         services.Configure<GithubConfiguration>(configuration.GetSection("Github"));
-
         services.AddSingleton<IGitHubClient>(sp =>
         {
             var config = sp.GetRequiredService<IOptions<GithubConfiguration>>();
@@ -26,15 +26,19 @@ public static class Dependencies
 
         services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(QueryGithubRequestHandler).Assembly));
 
-        return services.AddEndpointsApiExplorer()
-                       .AddSwaggerGen(options =>
-                       {
-                           options.SwaggerDoc("v1", new OpenApiInfo
-                           {
-                               Version = "v1",
-                               Title = "Github Plugin",
-                               Description = "Plugin to explain the code from Github and assist with its usage. Works with the provided Github file link."
-                           });
-                       });
+        services.AddEndpointsApiExplorer()
+                .AddSwaggerGen(options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Github Plugin",
+                        Description = "Plugin to explain the code from Github and assist with its usage. Works with the provided Github file link."
+                    });
+                });
+
+        services.AddTransient<IGithubService, GithubService>();
+
+        return services;
     }
 }
