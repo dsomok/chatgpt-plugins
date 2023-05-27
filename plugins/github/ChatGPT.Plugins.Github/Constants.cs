cdd2e8a981feda29bf810e414d73d66f1d37a0ea
Denis Somok <denis.somok@gmail.com>
2023-05-27 12:40:21 +0100

    Changes in system prompts

--- plugins/github/ChatGPT.Plugins.Github/Constants.cs
+++ plugins/github/ChatGPT.Plugins.Github/Constants.cs
@@ -2,7 +2,8 @@
 
 public static class Constants
 {
-    public const string REPOSITORY_STRUCTURE_HINT = "You should thoroughly analyze the repository structure to try to make some assumptions on the content of each file." +
-                                                    "To answer the users question select at least 15 most relevant files. Evaluate each file if it can contain an answer to user's question." +
+    public const string REPOSITORY_STRUCTURE_HINT = "You should thoroughly analyze the repository structure and try to make some assumptions on the content of each file." +
+                                                    "Create a plan to answer the users question. Then query the content of at least 15 files that are mostly relevant for this step." +
+                                                    "Evaluate each file if it can contain an answer to user's question. If you think that you need more information, request additional files." +
                                                     "Do not request file paths that are not present in the repository structure";
 }
