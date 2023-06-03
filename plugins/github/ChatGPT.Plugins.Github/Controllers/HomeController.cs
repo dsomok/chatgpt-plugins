4bd48ed340e96705416dc2bcb80057b9adc17abf
Denis Somok <denis.somok@gmail.com>
2023-06-04 00:08:26 +0100

    Added landing page

---
+++ plugins/github/ChatGPT.Plugins.Github/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatGPT.Plugins.Github.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    [Route("/")]
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var html = await System.IO.File.ReadAllTextAsync(@"./wwwroot/index.html", cancellationToken);
+        return Content(html, "text/html");
+    }
+}
