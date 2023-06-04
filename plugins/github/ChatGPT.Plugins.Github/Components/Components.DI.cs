43c72d22b7286a8dae22f39d37f6a3e8222062d3
Denis Somok <denis.somok@gmail.com>
2023-06-04 19:17:56 +0100

    Ordering response so that the csharp files are at the end

--- plugins/github/ChatGPT.Plugins.Github/Components/Components.DI.cs
+++ plugins/github/ChatGPT.Plugins.Github/Components/Components.DI.cs
@@ -14,6 +14,7 @@ internal static class Dependencies
                        .AddSingleton<IFileContentProcessor, UsingsRemover>()
                        .AddSingleton<IFileContentProcessor, FileContentMinifier>()
                        .AddSingleton<IResponseProcessor, ResponseLengthHardLimitProcessor>()
+                       .AddSingleton<IResponseProcessor, OrderingProcessor>()
                        .AddSingleton<IRepositoryStructureReducer, IgnoreInterfacesRepositoryStructureReducer>()
                        .AddSingleton<IRepositoryStructureReducer, HardLimitRepositoryStructureReducer>()
                        .AddSingleton<IGithubFilesEnumerator, GithubFilesEnumerator>()
---
+++ plugins/github/ChatGPT.Plugins.Github/Components/Response/OrderingProcessor.cs
@@ -0,0 +1,35 @@
+﻿using ChatGPT.Plugins.Github.Models;
+
+namespace ChatGPT.Plugins.Github.Components.Response;
+
+internal class OrderingProcessor : IResponseProcessor
+{
+    public Task<IList<GithubFile>> ProcessResponseAsync(IList<GithubFile> files, CancellationToken cancellationToken)
+    {
+        var result = files.OrderBy(file => file.Path, new GithubFilePathComparer()).ToList();
+        return Task.FromResult<IList<GithubFile>>(result);
+    }
+
+    private class GithubFilePathComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (IsCsharpFile(x))
+            {
+                return IsCsharpFile(y) ? 0 : 1;
+            }
+
+            if (IsMarkdownFile(x))
+            {
+                return IsCsharpFile(y)
+                    ? -1
+                    : IsMarkdownFile(y) ? 0 : 1;
+            }
+
+            return 0;
+        }
+
+        private bool IsCsharpFile(string path) => path.EndsWith(".cs");
+        private bool IsMarkdownFile(string path) => path.EndsWith(".md");
+    }
+}
